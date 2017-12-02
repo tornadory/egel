cdf378417b59a385636cea42be20b899294cba93
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2017-12-02 18:36:24 +0100

    more constants

--- src/math/MathConstants.ts
+++ src/math/MathConstants.ts
@@ -1,11 +1,7 @@
-/** @constant {number} */
 export const EPSILON = 0.000001;
 
-/** @constant {number} */
 export const QUARTER_PI = 0.7853981633974483;
 
-/** @constant {number} */
 export const HALF_PI = 1.5707963267948966;
 
-/** @constant {number} */
 export const DOUBLE_PI = 6.283185307179586;
