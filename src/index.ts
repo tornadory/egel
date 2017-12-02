504edb1a1a2627a219957be094e70d89dc7213ed
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2017-12-02 15:58:58 +0100

    fix import order

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,13 @@ import PerspectiveCamera from './camera/PerspectiveCamera';
 import * as Capabilities from './core/Capabilities';
 import * as Constants from './core/Constants';
 import * as Context from './core/Context';
+import * as CreateBuffer from './core/CreateBuffer';
+import Material from './core/Material';
 import Mesh from './core/Mesh';
 import Object3D from './core/Object3D';
+import Program from './core/Program';
 import Renderer from './core/Renderer';
 import Scene from './core/Scene';
-import Material from './core/Material';
 import Texture from './core/Texture';
 import VertexArrayObject from './core/VertexArrayObject';
 
@@ -24,9 +26,9 @@ import Geometry from './geometry/Geometry';
 
 // Math
 import Color from './math/Color';
+import * as Utilities from './math/Utilities';
 import Vector2 from './math/Vector2';
 import Vector3 from './math/Vector3';
-import * as Utilities from './math/Utilities';
 
 export {
     // Camera
