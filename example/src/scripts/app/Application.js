e8985cf12745bf6ec5ca5515f147bd4ef66a6367
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2018-01-04 20:04:11 +0100

    scaled bunny

--- example/src/scripts/app/Application.js
+++ example/src/scripts/app/Application.js
@@ -60,7 +60,7 @@ export default class Application {
 				});
 
 				const mesh = new Egel.Mesh(geometry, material);
-
+				mesh.scale.set(0.25, 0.25, 0.25);
 				scene.add(mesh);
 			})
 			.catch((error) => {
