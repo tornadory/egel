01ae82794f29015721c186744fdff9b52bdba8b8
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2018-01-24 09:10:16 +0100

    added textureCube

---
+++ src/core/TextureCube.ts
@@ -0,0 +1,112 @@
+// Core
+import * as Context from './Context';
+
+// Loaders
+import ImageLoader from '../loaders/ImageLoader';
+
+// Utilities
+import { createCanvas } from '../utilities/Canvas';
+import { warn } from '../utilities/Console';
+import EventDispatcher from '../utilities/EventDispatcher';
+
+let gl: WebGLRenderingContext;
+
+interface Options {
+    src?: string[];
+    magFilter?: number;
+    minFilter?: number;
+    wrapS?: number;
+    wrapT?: number;
+}
+
+export default class Texture extends EventDispatcher {
+    public src: string[];
+    public magFilter: number;
+    public minFilter: number;
+    public wrapS: number;
+    public wrapT: number;
+    public texture: WebGLTexture;
+    public loaders: any[];
+    public images: Array<HTMLCanvasElement | HTMLImageElement>;
+
+    constructor(options: Options) {
+        super();
+
+        gl = Context.get();
+
+        this.src = Array(6).fill('');
+        this.magFilter = gl.LINEAR;
+        this.minFilter = gl.LINEAR;
+        this.wrapS = gl.CLAMP_TO_EDGE;
+        this.wrapT = gl.CLAMP_TO_EDGE;
+
+        Object.assign(this, options);
+
+        const { canvas } = createCanvas(1, 1);
+
+        this.texture = gl.createTexture();
+        this.images = [];
+        this.loaders = [];
+
+        const images = [];
+
+        for (let i = 0; i < 6; i++) {
+            images.push(canvas);
+        }
+
+        this.update(images);
+
+        this.src.forEach((src, i) => {
+            this.loaders[i] = this.load(this.src[i]);
+        });
+
+        Promise.all(this.loaders)
+            .then(this.onTextureLoaded)
+            .catch(this.onTextureError);
+    }
+
+    public load(src: string) {
+        return ImageLoader(src);
+    }
+
+    public onTextureLoaded = (response: Array<HTMLCanvasElement | HTMLImageElement>) => {
+        this.images = response;
+        this.update(this.images);
+        this.emit('loaded');
+    }
+
+    public onTextureError = (error: string) => {
+        warn(error);
+        this.emit('error', error);
+    }
+
+    public update(images: Array<HTMLCanvasElement | HTMLImageElement>) {
+        gl = Context.get();
+
+        gl.bindTexture(gl.TEXTURE_CUBE_MAP, this.texture);
+
+        const targets = [
+            gl.TEXTURE_CUBE_MAP_POSITIVE_X,
+            gl.TEXTURE_CUBE_MAP_NEGATIVE_X,
+            gl.TEXTURE_CUBE_MAP_POSITIVE_Y,
+            gl.TEXTURE_CUBE_MAP_NEGATIVE_Y,
+            gl.TEXTURE_CUBE_MAP_POSITIVE_Z,
+            gl.TEXTURE_CUBE_MAP_NEGATIVE_Z,
+        ];
+
+        for (let i = 0; i < 6; i += 1) {
+            const image = images[i];
+
+            gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
+
+            gl.texImage2D(targets[i], 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+            gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, this.magFilter);
+            gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, this.minFilter);
+            gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, this.wrapS);
+            gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, this.wrapT);
+        }
+
+        gl.bindTexture(gl.TEXTURE_CUBE_MAP, null);
+    }
+}
